e9d7f34df2df09983cbec839df7287b8f609d680
Nicole Tastula <ntastula@gmail.com>
2025-10-10 13:12:52 +1100

    Added vercel address to cors_allowed_origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -146,7 +146,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://your-frontend-name.vercel.app",
+    "https://nlp-insights-capstone-nt-deploy.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
