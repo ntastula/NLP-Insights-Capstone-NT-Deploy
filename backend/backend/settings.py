53a23bdd68e19ac1b0b222d34631a73afe4e647e
Nicole Tastula <ntastula@gmail.com>
2025-10-10 15:45:22 +1100

    Reset build

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -148,6 +148,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://nlp-insights-capstone-nt-deploy.vercel.app",
 ]
+
 CORS_ALLOW_CREDENTIALS = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
